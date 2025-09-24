38c345cc6357e6d76b8f31b86aa2da1a03d2455e
giraldo32 <elber.mendoza@est.iudigital.edu.co>
2025-09-23 23:32:10 -0500

    actualizando

--- models/media.js
+++ models/media.js
@@ -1,10 +1,23 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require('mongoose');
 
 const MediaSchema = Schema({
-    nombre: {
+    titulo: {
         type: String,
-        required: [true, 'El nombre es obligatorio'],
+        required: [true, 'El título es obligatorio'],
         unique: true
+    },
+    sinopsis: {
+        type: String,
+        required: [true, 'La sinopsis es obligatoria']
+    },
+    url: {
+        type: String,
+        required: [true, 'La URL es obligatoria'],
+        unique: true
+    },
+    anioEstreno: {
+        type: Number,
+        required: [true, 'El año de estreno es obligatorio']
     },
     estado: {
         type: Boolean,
@@ -18,12 +31,7 @@ const MediaSchema = Schema({
     fechaActualizacion: {
         type: Date,
         default: new Date()
-    },
-    url: {
-        type: String,
-        required: [true, 'La URL es obligatoria'],
-        unique: true
     }
-})
+});
 
-module.exports = model('Media', MediaSchema)
+module.exports = model('Media', MediaSchema);
